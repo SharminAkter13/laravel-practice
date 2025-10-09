d922fd355b88a65ba84099a99bee6feb87c6ffae
SharminAkter13 <sharminakter13092000@gmail.com>
2025-10-09 23:40:02 +0600

    laravel

--- laravel-project/app/Http/Controllers/CategoryController.php
+++ laravel-project/app/Http/Controllers/CategoryController.php
@@ -18,7 +18,7 @@ class CategoryController extends Controller
     
        public function create()
     {
-        return view('create');
+        return view('pages.create');
     }
 
     public function store(Request $request)
